9f1acdcdd3741044ec96421d9d10a49c73c12378
Clechay <luke.clechay@gmail.com>
2019-12-19 18:15:38 +0100

    init

---
+++ script.js
@@ -0,0 +1,13 @@
+/**
+ * @type {HTMLElement}
+ */
+const wrap = document.querySelector("#wrap");
+/**
+ * @type {HTMLElement}
+ */
+const ui = document.querySelector("#ui");
+/**
+ * @type {HTMLCanvasElement}
+ */
+const canvas = document.querySelector("#game");
+const ctx = canvas.getContext("2d");
